3610c399e73738ee920db7a773000fce8a8a3a1c
tsensei <tjsiam019@gmail.com>
2022-12-05 20:10:09 +0600

    Refactor MAC Changer

--- mac_changer.py
+++ mac_changer.py
@@ -2,6 +2,9 @@
 
 import subprocess
 import optparse
+import re
+
+# Parser input options and arguments
 
 def get_arguments():
 	parser = optparse.OptionParser()
@@ -14,6 +17,8 @@ def get_arguments():
 		parser.error("[-] Please specify a new mac, use --help for more info.")
 	else:
 		return options
+
+# Changes MAC address using system calls
 	
 def change_mac(interface, new_mac):
 	print("[+] Changing MAC address for " + interface + " to " + new_mac)
@@ -22,9 +27,32 @@ def change_mac(interface, new_mac):
 	subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
 	subprocess.call(["ifconfig", interface, "up"])
 
+# Returns the current MAC address
+
+def get_current_mac(interface):
+	ifconfig_result = subprocess.check_output(["ifconfig", interface]).decode('utf-8')
+	current_mac = re.search(r'\w\w:\w\w:\w\w:\w\w:\w\w:\w\w', ifconfig_result)
+	if current_mac:
+		return current_mac.group(0)
+	else:
+		print("[-] Could not read MAC address")
+		
+		
+		
+# Main Functionalities
 
 options = get_arguments()
+
+current_mac = get_current_mac(options.interface)
+print("[+] Current MAC : " + str(current_mac))
+
 change_mac(options.interface, options.new_mac)
+
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac:
+	print("[+] MAC address was successfully changed to " + current_mac)
+else:
+	print("[-] MAC address could not be changed")
 
 
 
